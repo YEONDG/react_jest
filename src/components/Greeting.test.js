ef7f951fd7d21ab9cacaeb1b31d8aff0398ee5f1
YDG <zzmn1234@naver.com>
2022-06-06 17:29:49 +0900

    click state test

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,14 +1,53 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Greeting from './Greeting';
 
-test('renders Hello World ad a text', () => {
-  //Arrange
-  render(<Greeting />);
+// Suites 그룹화
+describe('Greeting componenet', () => {
+  test('renders Hello World ad a text', () => {
+    //Arrange
+    render(<Greeting />);
 
-  // Act
-  // ...nothing
+    // Act
+    // ...nothing
 
-  // Assert
-  const helloWorldElement = screen.getByText('Hello World');
-  expect(helloWorldElement).toBeInTheDocument();
+    // Assert
+    const helloWorldElement = screen.getByText('Hello World', { exact: false });
+    expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test("renders It's good button was NOT clicked", () => {
+    //Arrange
+    render(<Greeting />);
+    // Act
+    //...nothing
+
+    // Assert
+    const outputElement = screen.getByText("It's good", { exact: false });
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test('renders Changed! if the button was clicked', () => {
+    //Arrange
+    render(<Greeting />);
+    // Act
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement);
+
+    // Assert
+    const outputElement = screen.getByText('Changed!', { exact: false });
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test("does not render It's good if the button was clicked", () => {
+    //Arrange
+    render(<Greeting />);
+    // Act
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement);
+
+    // Assert
+    const outputElement = screen.queryByText("It's good", { exact: false });
+    expect(outputElement).toBeNull();
+  });
 });
